59cc817993fd9e9fdbc12dcbaa53051334fcbb48
Funkschy <felix.schoeller@protonmail.com>
2020-10-15 14:29:22 +0200

    init

---
+++ Makefile
@@ -0,0 +1,65 @@
+SHELL := /bin/bash
+
+BIN_NAME = kantan
+K_FILES = src/cli/opt.kan \
+		  src/cli/config.kan \
+		  src/codegen/target.kan \
+		  src/main.kan \
+		  src/memory/arena.kan \
+		  src/source/ident.kan \
+		  src/source/span.kan \
+		  src/std/cdeps.kan \
+		  src/std/dbg.kan \
+		  src/std/files/impl.kan \
+		  src/std/files/path.kan \
+		  src/std/files/unix.kan \
+		  src/std/libc.kan \
+		  src/std/map.kan \
+		  src/std/num.kan \
+		  src/std/ptrvec.kan \
+		  src/std/str.kan \
+		  src/std/vec.kan \
+		  src/std/vmap.kan \
+		  src/types/data.kan \
+		  src/types/primitive.kan \
+		  src/types/subst.kan \
+		  src/types/types.kan
+
+C_FILES = lib.c
+C_OBJ_FILES = $(C_FILES:.c=.c.o)
+START_FOLDER = $(shell pwd)
+
+LLVM_PATH = $(HOME)/Downloads/llvm/llvm-10.0.0.src/build
+LLVM_CONFIG = $(LLVM_PATH)/bin/llvm-config
+
+LLVM_LIB_NAMES = x86codegen webassemblycodegen passes
+
+LLVM_C_FLAGS = $(shell $(LLVM_CONFIG) --cflags)
+LLVM_LD_FLAGS = $(shell $(LLVM_CONFIG) --ldflags)
+LLVM_LIBS = $(shell $(LLVM_CONFIG) --libs $(LLVM_LIB_NAMES))
+LLVM_SYS_LIBS = $(shell $(LLVM_CONFIG) --system-libs)
+
+LD_FLAGS = $(LLVM_LD_FLAGS) -fdata-sections -ffunction-sections
+LIBS = $(LLVM_LIBS) $(LLVM_SYS_LIBS) -Wl,--gc-sections
+
+KANTAN_STABLE = $(START_FOLDER)/../kantan -g
+KANTAN_KANTAN_MEMCHECK = valgrind --leak-check=full --suppressions=$(START_FOLDER)/suppress-llvm-errors.supp $(START_FOLDER)/compiler
+KANTAN_KANTAN_MASSIF = valgrind --tool=massif --massif-out-file=../massif.out $(START_FOLDER)/compiler
+KANTAN_KANTAN = $(KANTAN_KANTAN_MEMCHECK) -g
+
+$(BIN_NAME) : $(K_FILES) $(C_OBJ_FILES)
+	if $(KANTAN_STABLE) $(K_FILES) -o out.o; then \
+		g++ $(LD_FLAGS) -o $(BIN_NAME) out.o $(C_OBJ_FILES) $(LIBS); \
+		rm out.o ; \
+		rm $(C_OBJ_FILES) ; \
+	fi
+
+$(C_OBJ_FILES) : $(C_FILES)
+	for file in $(C_FILES) ; do \
+		gcc -O3 -Wall -c $$file -o $(addsuffix .o, $$file); \
+	done
+
+.PHONY: clean
+clean :
+	rm $(BIN_NAME) ; \
+	rm *.o
---
+++ lib.c
@@ -0,0 +1,189 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <errno.h>
+#include <sys/stat.h>
+
+#define DEBUG_ASSERT 1
+
+// forward decls
+ssize_t vformat_str(char **dest, char const *fmt, va_list args);
+
+void assert_fmt(bool condition, char const *fmt, ...) {
+#if DEBUG_ASSERT
+    if (condition) {
+        return;
+    }
+
+    char *s = NULL;
+    va_list args;
+    va_start(args, fmt);
+    ssize_t size = vformat_str(&s, fmt, args);
+    va_end(args);
+
+    if (size < 0) {
+        return;
+    }
+
+    printf("%s\n", s);
+    free(s);
+
+    abort();
+#endif
+}
+
+char const *const l_format_str(size_t *len, char const *fmt, ...) {
+    char *s = NULL;
+    va_list args;
+    va_start(args, fmt);
+    ssize_t size = vformat_str(&s, fmt, args);
+    va_end(args);
+
+    if (size < 0) {
+        return NULL;
+    }
+
+    *len = size;
+    return s;
+}
+
+char const *const format_str(char const *fmt, ...) {
+    char *s = NULL;
+    va_list args;
+    va_start(args, fmt);
+    ssize_t size = vformat_str(&s, fmt, args);
+    va_end(args);
+
+    if (size < 0) {
+        return NULL;
+    }
+
+    return s;
+}
+
+size_t read_char(char const *string, size_t s_len, int32_t *ch) {
+    int32_t c_len = 1;
+
+    if ((*string & 0xf8) == 0xf0) {
+        c_len = 4;
+    } else if ((*string & 0xf0) == 0xe0) {
+        c_len = 3;
+    } else if ((*string & 0xe0) == 0xc0) {
+        c_len = 2;
+    }
+
+    if (c_len > s_len) {
+        return s_len;
+    }
+
+    if (ch != NULL) {
+        memcpy(ch, string, c_len);
+    }
+
+    return (size_t) c_len;
+}
+
+bool is_file(char const *path) {
+    struct stat s;
+    if (stat(path, &s) == 0) {
+        return s.st_mode & S_IFREG;
+    }
+    return false;
+}
+
+// these have to match src/std/files/path.Error
+#define ERROR_COULD_NOT_OPEN_FILE       2
+#define ERROR_COULD_NOT_ALLOCATE_BUFFER 3
+#define ERROR_COULD_NOT_READ_FILE       4
+
+int32_t read_file(char const *path, char const **content, size_t *len) {
+    if (!is_file(path)) {
+        // TODO: custom error, and check if this works on windows
+        return ERROR_COULD_NOT_OPEN_FILE;
+    }
+
+    FILE *file = fopen(path, "r");
+    if (file == NULL) {
+        return ERROR_COULD_NOT_OPEN_FILE;
+    }
+
+    fseek(file, 0L, SEEK_END);
+    size_t file_size = (size_t) ftell(file);
+    rewind(file);
+
+    char *buffer = malloc(file_size + 1);
+
+    if (buffer == NULL) {
+        fclose(file);
+        return ERROR_COULD_NOT_ALLOCATE_BUFFER;
+    }
+
+    size_t bytes_read = fread(buffer, sizeof(char), file_size, file);
+
+    if (bytes_read < file_size) {
+        fclose(file);
+        free(buffer);
+        return ERROR_COULD_NOT_READ_FILE;
+    }
+
+    buffer[bytes_read] = '\0';
+
+    fclose(file);
+    *content = buffer;
+    *len = file_size;
+
+    return 0;
+}
+
+ssize_t vformat_str(char **dest, char const *fmt, va_list args){
+    int size = 0;
+    va_list tmp_args;
+
+    // vsnprintf modifies the arg list, so we have to copy it here
+    va_copy(tmp_args, args);
+
+    // pass NULL as str, so that we just get the size
+    size = vsnprintf(NULL, 0, fmt, tmp_args);
+
+    va_end(tmp_args);
+
+    if (size < 0) {
+        return size;
+    }
+
+    char *str = malloc(size + 1);
+    if (str == NULL) {
+        return -1;
+    }
+    size = vsprintf(str, fmt, args);
+    *dest = str;
+
+    return size;
+}
+
+size_t int_num_digits(size_t i) {
+    // pass NULL as str, so that we just get the size
+    return snprintf(NULL, 0, "%lu", i);
+}
+
+size_t ptr_to_int(void* ptr) {
+    return (size_t) ptr;
+}
+
+void *int_to_ptr(size_t i) {
+    size_t s = i;
+    return (void *)s;
+}
+
+void get_sys(bool *is_linux, bool *is_darwin, bool *is_win32) {
+#if defined(linux) || defined(__linux__)
+    if (is_linux) *is_linux = true;
+#elif defined(darwin) || defined(__APPLE__)
+    if (is_darwin) *is_darwin = true;
+#elif defined(WIN32) || defined(_WIN32)
+    if (is_win32) *is_win32 = true;
+#endif
+}
