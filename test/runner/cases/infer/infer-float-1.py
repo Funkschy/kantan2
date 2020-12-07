205435e0bc5cc8b575e24d742b2c253b0e1d46ee
Funkschy <felix.schoeller@protonmail.com>
2020-12-07 16:20:55 +0100

    added proper type inference for floats

---
+++ test/runner/cases/infer/infer-float-1.py
@@ -0,0 +1,53 @@
+from typing import Optional
+
+from runner.output import Output
+from runner.testcase import SuccessTestCase, TestError, expected_but_got, kantan_filename
+
+expected_modules = [{
+    'kind': 'module',
+    'path': kantan_filename(__file__),
+    'items': [
+        {
+            "kind": "func_def",
+            "name": "main",
+            "ret": "void",
+            "body": {
+                "kind": "block",
+                "stmts": [
+                    {
+                        "kind": "local_var_decl",
+                        "name": "f",
+                        "ty": None,
+                        "value": {
+                            "kind": "literal",
+                            "value": 0.0
+                        }
+                    },
+                    {
+                        "kind": "expr_stmt",
+                        "expr": {
+                            "kind": "identifier",
+                            "value": "f",
+                            "ty": "f32"
+                        }
+                    },
+                ]
+            }
+        }
+    ]
+}]
+
+
+class Test(SuccessTestCase):
+    def __init__(self, executor):
+        super().__init__(executor)
+        self.options.append('--dump-ast')
+
+    def test_output(self, output: Output) -> Optional[TestError]:
+        super_error = super().test_output(output)
+        if super_error is not None:
+            return super_error
+
+        modules = output.modules
+        if modules != expected_modules:
+            return expected_but_got('modules', expected_modules, modules)
